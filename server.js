19685105b12ac63c013845d9101c3263fc52ff80
insta-api <154006537+insta-api@users.noreply.github.com>
2023-12-17 10:19:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const PORT = 8080;
 app.use(bodyParser.json());
 app.use(cors());
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://insta-loader519.web.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.options('*', cors()); // Enable preflight for all routes
+
 app.get('/', (req, res) => {
     res.redirect('http://localhost:3000');
 });
@@ -22,7 +32,7 @@ app.post('/download', async (req, res) => {
     if (!postUrl) {
       return res.status(400).json({ error: 'Invalid post URL.' });
     }
-
+    console.log(postUrl)
     browser = await puppeteer.launch({ headless: 'new' });
     const page = await browser.newPage();
 
@@ -32,10 +42,11 @@ app.post('/download', async (req, res) => {
 
     const videoTagContent = await page.evaluate(() => {
       const videoTag = document.querySelector('video'); // Assuming there is a video tag on the page
-            const imageTag = document.querySelector('img'); // Assuming there is an image tag on the page
-            return videoTag ? videoTag.src : (imageTag ? imageTag.outerHTML : 'No video or image tag found');
+      const imageTag = document.querySelector('img'); // Assuming there is an image tag on the page
+      return videoTag ? videoTag.src : (imageTag ? imageTag.outerHTML : 'No video or image tag found');
     });
-// Send the video or image tag content as the response
+
+    // Send the video or image tag content as the response
     res.status(200).json({ content: videoTagContent });
   } catch (error) {
     console.error(error);
@@ -46,7 +57,6 @@ app.post('/download', async (req, res) => {
     }
   }
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
