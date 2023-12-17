0414a4e4a79a7e8a5c7ac1ad90f93b795b515e12
insta-api <154006537+insta-api@users.noreply.github.com>
2023-12-17 10:09:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,52 @@
 const express = require('express');
 const cors = require('cors');
 const puppeteer = require('puppeteer');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 8080;
 
+app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.redirect('http://localhost:3000');
 });
 
-app.get('/download/:postUrl', async (req, res) => {
-    let browser; // Declare the browser variable outside the try block
+app.post('/download', async (req, res) => {
+  let browser;
 
-    try {
-        const postUrl = req.params.postUrl;
-        browser = await puppeteer.launch({ headless: 'new' }); // Explicitly set headless mode to "new"
-        const page = await browser.newPage();
+  try {
+    const postUrl = req.body.postUrl; // Assuming you're using body-parser middleware or similar
 
-        // Navigate to the Instagram post URL
-        await page.goto(postUrl); // Use the dynamic postUrl variable
+    if (!postUrl) {
+      return res.status(400).json({ error: 'Invalid post URL.' });
+    }
 
-        await page.waitForTimeout(5000);
+    browser = await puppeteer.launch({ headless: 'new' });
+    const page = await browser.newPage();
 
-        const videoTagContent = await page.evaluate(() => {
-            const videoTag = document.querySelector('video'); // Assuming there is a video tag on the page
+    // Navigate to the Instagram post URL
+    await page.goto(postUrl);
+    await page.waitForTimeout(5000);
+
+    const videoTagContent = await page.evaluate(() => {
+      const videoTag = document.querySelector('video'); // Assuming there is a video tag on the page
             const imageTag = document.querySelector('img'); // Assuming there is an image tag on the page
             return videoTag ? videoTag.src : (imageTag ? imageTag.outerHTML : 'No video or image tag found');
-        });
-        // Send the video or image tag content as the response
-        res.status(200).json({ content: videoTagContent });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    } finally {
-        // Close the browser when done
-        if (browser) {
-            await browser.close();
-        }
+    });
+// Send the video or image tag content as the response
+    res.status(200).json({ content: videoTagContent });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+    if (browser) {
+      await browser.close();
     }
+  }
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
