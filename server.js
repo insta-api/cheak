587071dffd7f405cb0071e6957def1867af2b664
insta-api <154006537+insta-api@users.noreply.github.com>
2023-12-18 11:28:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,12 +18,8 @@ app.get('/', (req, res) => {
 
 app.get('/install', (req, res) => {
     const installCommand =
-        'apt-get update && ' +
-        'apt-get install -y wget gnupg && ' +
-        'wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && ' +
-        'sh -c \'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list\' && ' +
-        'apt-get update && ' +
-        'apt-get install -y google-chrome-stable';
+        'brew update && ' +
+        'brew install --cask google-chrome';
 
     exec(installCommand, (error, stdout, stderr) => {
         if (error) {
