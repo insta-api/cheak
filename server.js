be9738c282da1bd0170b14ad9471d7cdae9f0979
insta-api <154006537+insta-api@users.noreply.github.com>
2023-12-19 15:37:39 +0530

    Update old code wo mangodb server.js

--- server.js
+++ server.js
@@ -3,22 +3,6 @@ const cors = require('cors');
 const puppeteer = require('puppeteer');
 const bodyParser = require('body-parser');
 const path = require('path');
-const mongoose = require('mongoose');
-const { exec } = require('child_process');
-const os = require('os');
-
-mongoose.connect('mongodb+srv://whitehunter513:CfIm5yP9OiMQSg6j@ters.z8qdcrl.mongodb.net/insta-details', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const instaDataSchema = new mongoose.Schema({
-    ipaddress: String,
-    url: String,
-    mobilename: String,
-    userAgent: String,
-    platform: String,
-    language: String,
-});
-
-const instaData = mongoose.model('insta-datas', instaDataSchema);
 
 const app = express();
 const PORT = 8080;
@@ -57,29 +41,16 @@ app.get('/install', (req, res) => {
     });
 });
 
-app.get('/get-os-info', (req, res) => {
-    exec('which yum', (error, stdout, stderr) => {
-        const yumAvailable = !error;
-
-        const osInfo = {
-            platform: os.platform(),
-            type: os.type(),
-            release: os.release(),
-            architecture: os.arch(),
-            cpus: os.cpus(),
-            totalMemory: os.totalmem(),
-            freeMemory: os.freemem(),
-            yumAvailable: yumAvailable,
-        };
-
-        res.json(osInfo);
-    });
-});
-
 app.post('/download', async (req, res) => {
-    
-    const ipAddress = req.clientIp;
-    const browser = await puppeteer.launch({
+    let browser;
+
+    try {
+        const postUrl = req.body.postUrl;
+
+        if (!postUrl) {
+            return res.status(400).json({ error: 'Invalid post URL.' });
+        }
+        const browser = await puppeteer.launch({
             args: [
               "--disable-setuid-sandbox",
               "--no-sandbox",
@@ -91,32 +62,7 @@ app.post('/download', async (req, res) => {
                 ? process.env.PUPPETEER_EXECUTABLE_PATH
                 : puppeteer.executablePath(),
           });
-   
-    try {
-        let postUrl = req.body.postUrl;
-        let deviceDetail = req.body.device;
-
-        if (postUrl.includes('/?')) {
-            postUrl = postUrl.split('/?')[0];
-        }
-
-        const instaDataInstance = new instaData({
-            ipaddress: ipAddress,
-            url: postUrl,
-            mobilename: deviceDetail.mobilename,
-            userAgent: deviceDetail.userAgent,
-            platform: deviceDetail.platform,
-            language: deviceDetail.language,
-        });
-
-        // Save the instance to the database
-        await instaDataInstance.save();
-
-        if (!postUrl) {
-            return res.status(400).json({ error: 'Invalid post URL.' });
-        }
-
-        browser = await puppeteer.launch({ headless: 'new' });
+       
         const page = await browser.newPage();
 
         // Navigate to the Instagram post URL
@@ -140,6 +86,7 @@ app.post('/download', async (req, res) => {
         }
     }
 });
+
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 });
